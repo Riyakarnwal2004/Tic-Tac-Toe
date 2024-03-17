8416d5a61d2d4f519a62c26021280909e4c74ccd
Riya Karnwal <riyakarnwal2004@gmail.com>
2024-03-17 22:17:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let boxtext = document.getElementsByClassName('boxtext');
 let turn=0;
 Array.from(document.getElementsByClassName('boxtext')).forEach((element)=>{
     element.addEventListener('click',(e)=>{
+        
         if(turn==0){
             e.target.innerText="x";
         }
