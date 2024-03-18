7157848aff1e9bfe169ab2cd59f4088dfdf408a4
Riya Karnwal <riyakarnwal2004@gmail.com>
2024-03-18 12:43:29 +0530

    update

--- script.js
+++ script.js
@@ -1,16 +1,63 @@
-let boxtext = document.getElementsByClassName('boxtext');
-let turn=0;
-Array.from(document.getElementsByClassName('boxtext')).forEach((element)=>{
-    element.addEventListener('click',(e)=>{
-        
-        if(turn==0){
-            e.target.innerText="x";
-        }
-        else{
-            e.target.innerText="O";
-        }
-        turn=!(turn);
+let music=new Audio("music.mp3");
+let audioturn=new Audio("ting.mp3");
+let audiogameover= new Audio("gameover.mp3");
+let turn = "X";
+let gameover=false;
 
+const changeTurn=()=>{
+    return turn==="X"? "0":"X";
+}
+
+const checkwin=()=>{
+    let boxtext=document.getElementsByClassName("boxtext");
+ let wins=[
+    [0,1,2,3.5,4.5,0],
+    [3,4,5,3.5,13.5,0],
+    [6,7,8,3.5,22.5,0],
+    [0,3,6,-5.5,13.5,90],
+    [1,4,7,3.5,13.5,90],
+    [2,5,8,12.5, 13.5,90],
+    [0,4,8,3.5,13.5,45],
+    [2,4,6,3.5,13.5,135],
+ ]
+ wins.forEach(e=>{
+
+    if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
+        document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
+        gameover = true
+        document.querySelector('.img').getElementsByTagName('img')[0].style.width="200px";
+        document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+            document.querySelector(".line").style.width = "20vw";  
+    }
+ 
+ })
+}
+let boxes=document.getElementsByClassName("box");
+Array.from(boxes).forEach(element=>{
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click',(e)=>{
+    if(boxtext.innerText===""){
+       boxtext.innerText=turn;
+        turn =changeTurn();
+        audioturn.play();
+        checkwin();
+        if(!gameover){
+        document.getElementsByClassName("info")[0].innerText="Turn for" + turn;
+        }
+    }
+  })
+})
+
+reset.addEventListener('click',()=>{
+    let boxtext=document.querySelectorAll('.boxtext');
+    Array.from(boxtext).forEach(element=>{
+        element.innerText=" "
     })
-    
+    turn="X";
+    gameover=false
+    document.getElementsByClassName("info")[0].innerText="Turn for" + turn;
+       
+    document.querySelector('.img').getElementsByTagName('img')[0].style.width="0px";
+    document.querySelector(".line").style.width = "0vw";  
+
 })
